5546c7c7d40088056a866573d4b6d563c9032e30
ADOPCIÓN Y VENTA DE MASCOTAS <ospinazabalamariapaula@gmail.com>
2026-05-11 18:29:15 -0500

    cambio de tests

--- test/test_abonos.py
+++ test/test_abonos.py
@@ -12,39 +12,39 @@ class TestAbonos(unittest.TestCase):
         AbonosController.borrar_tabla()
         AbonosController.crear_tabla()
 
-    def test_insertar_1(self):
+    def test_1_insertar_1(self):
         abono = Abono(id_abono=None, id_plan=1, mes_abono=24,
                       valor_abono=2000000, nueva_cuota=296587.78)
         AbonosController.insertar(abono)
         buscado = AbonosController.buscar(1)
         self.assertTrue(abono.is_equal(buscado))
 
-    def test_insertar_2(self):
+    def test_2_insertar_2(self):
         abono = Abono(id_abono=None, id_plan=2, mes_abono=12,
                       valor_abono=1000000, nueva_cuota=319805.90)
         AbonosController.insertar(abono)
         buscado = AbonosController.buscar(2)
         self.assertTrue(abono.is_equal(buscado))
 
-    def test_insertar_3(self):
+    def test_3_insertar_3(self):
         abono = Abono(id_abono=None, id_plan=1, mes_abono=36,
                       valor_abono=5000000, nueva_cuota=359226.16)
         AbonosController.insertar(abono)
         buscado = AbonosController.buscar(3)
         self.assertTrue(abono.is_equal(buscado))
 
-    def test_modificar_1(self):
+    def test_4_modificar_1(self):
         abono = Abono(id_abono=1, id_plan=1, mes_abono=24,
                       valor_abono=3000000, nueva_cuota=222440.84)
         AbonosController.modificar(abono)
         buscado = AbonosController.buscar(1)
         self.assertTrue(abono.is_equal(buscado))
 
-    def test_eliminar_1(self):
+    def test_5_eliminar_1(self):
         AbonosController.eliminar(3)
         self.assertRaises(Exception, AbonosController.buscar, 3)
 
-    def test_error_abono_inexistente(self):
+    def test_6_error_abono_inexistente(self):
         self.assertRaises(Exception, AbonosController.buscar, 9999)
 
 if __name__ == '__main__':
--- test/test_planes.py
+++ test/test_planes.py
@@ -12,7 +12,7 @@ class TestPlanes(unittest.TestCase):
         PlanesController.borrar_tabla()
         PlanesController.crear_tabla()
 
-    def test_insertar_1(self):
+    def test_1_insertar_1(self):
         plan = PlanAhorro(id_plan=None, cedula="123456", meta=10000000,
                           tasa_interes=0.01, plazo=24, cuota_mensual=370734.72,
                           fecha_creacion="2026-05-11")
@@ -20,7 +20,7 @@ class TestPlanes(unittest.TestCase):
         buscado = PlanesController.buscar(1)
         self.assertTrue(plan.is_equal(buscado))
 
-    def test_insertar_2(self):
+    def test_2_insertar_2(self):
         plan = PlanAhorro(id_plan=None, cedula="654321", meta=5000000,
                           tasa_interes=0.0075, plazo=12, cuota_mensual=399757.38,
                           fecha_creacion="2026-05-11")
@@ -28,7 +28,7 @@ class TestPlanes(unittest.TestCase):
         buscado = PlanesController.buscar(2)
         self.assertTrue(plan.is_equal(buscado))
 
-    def test_insertar_3(self):
+    def test_3_insertar_3(self):
         plan = PlanAhorro(id_plan=None, cedula="123456", meta=20000000,
                           tasa_interes=0.0083, plazo=36, cuota_mensual=478968.21,
                           fecha_creacion="2026-05-11")
@@ -36,7 +36,7 @@ class TestPlanes(unittest.TestCase):
         buscado = PlanesController.buscar(3)
         self.assertTrue(plan.is_equal(buscado))
 
-    def test_modificar_1(self):
+    def test_4_modificar_1(self):
         plan = PlanAhorro(id_plan=1, cedula="123456", meta=10000000,
                           tasa_interes=0.01, plazo=36, cuota_mensual=232062.38,
                           fecha_creacion="2026-05-11")
@@ -44,11 +44,11 @@ class TestPlanes(unittest.TestCase):
         buscado = PlanesController.buscar(1)
         self.assertTrue(plan.is_equal(buscado))
 
-    def test_eliminar_1(self):
+    def test_5_eliminar_1(self):
         PlanesController.eliminar(3)
         self.assertRaises(Exception, PlanesController.buscar, 3)
 
-    def test_error_plan_inexistente(self):
+    def test_6_error_plan_inexistente(self):
         self.assertRaises(Exception, PlanesController.buscar, 9999)
 
 if __name__ == '__main__':
