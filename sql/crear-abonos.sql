e124a6f433a57095459c441b3fbfce23a95b87b8
ADOPCIÓN Y VENTA DE MASCOTAS <ospinazabalamariapaula@gmail.com>
2026-05-16 15:52:18 -0500

    cambio

--- sql/crear-abonos.sql
+++ sql/crear-abonos.sql
@@ -6,8 +6,3 @@ create table if not exists abonos (
   nueva_cuota decimal not null
 );
 
-INSERT INTO abonos (id_plan, mes_abono, valor_abono, nueva_cuota)
-VALUES (1, 3, 500000, 215000.00);
-
-INSERT INTO abonos (id_plan, mes_abono, valor_abono, nueva_cuota)
-VALUES (2, 1, 200000, 380000.00);
