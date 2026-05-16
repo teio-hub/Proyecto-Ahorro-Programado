a84162968bc4441cce9b4633412506fd293dee89
ADOPCIÓN Y VENTA DE MASCOTAS <ospinazabalamariapaula@gmail.com>
2026-05-16 15:45:30 -0500

    se agregan los insert

--- sql/crear-abonos.sql
+++ sql/crear-abonos.sql
@@ -5,3 +5,9 @@ create table if not exists abonos (
   valor_abono decimal not null,
   nueva_cuota decimal not null
 );
+
+INSERT INTO abonos (id_plan, mes_abono, valor_abono, nueva_cuota)
+VALUES (1, 3, 500000, 215000.00);
+
+INSERT INTO abonos (id_plan, mes_abono, valor_abono, nueva_cuota)
+VALUES (2, 1, 200000, 380000.00);
